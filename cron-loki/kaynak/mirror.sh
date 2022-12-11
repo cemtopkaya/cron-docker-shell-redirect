7ee0793b227fa290373ed838592f7ac70696a0d4
Cem Topkaya <cem.topkaya@hotmail.com>
2022-12-12 00:37:07 +0300

    Update mirror.sh

--- cron-loki/kaynak/mirror.sh
+++ cron-loki/kaynak/mirror.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 json='{"tag": "mirror", "mesaj": "gitlab mirror mesajı"}'
-while(true); do sleep 2; echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2;  done;
+# while(true); do sleep 2; echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2;  done;
+echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2
