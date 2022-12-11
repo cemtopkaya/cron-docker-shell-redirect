43a2cf0876973912d4fe4dd38bb27a4786eebfe6
Cem Topkaya <cem.topkaya@hotmail.com>
2022-12-12 00:37:37 +0300

    Update calistir.sh

--- cron-loki/kaynak/calistir.sh
+++ cron-loki/kaynak/calistir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 json='{"tag": "redmine-clone", "mesaj": "redmine clone oldu"}'
-while(true); do sleep 1; echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2;  done;
+#while(true); do sleep 1; echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2;  done;
+echo "$json"  > /proc/1/fd/1 2>/proc/1/fd/2
